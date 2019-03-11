13cab3db646c6f47bb53935e03623c711c3f9ab3
Jackie Diu <jackiediu@gmail.com>
2019-03-11 13:00:49 -0700

    added expense icon

--- src/conponents/expenseResult.jsx
+++ src/conponents/expenseResult.jsx
@@ -8,17 +8,26 @@ class ExpenseResult extends React.Component {
           <ul>
             <li>
               <h4>BUDGET</h4>
-              <img src="" alt="budget money" />
+              <img
+                src="https://img.icons8.com/bubbles/100/000000/cash-in-hand.png"
+                alt="budget money"
+              />
               <p />
             </li>
             <li>
               <h4>EXPENSES</h4>
-              <img src="" alt="budget expenses" />
+              <img
+                src="https://img.icons8.com/bubbles/100/000000/sim-card-chip.png"
+                alt="budget expenses"
+              />
               <p />
             </li>
             <li>
               <h4>BALANCE</h4>
-              <img src="" alt="budget balance" />
+              <img
+                src="https://img.icons8.com/bubbles/100/000000/money-bag.png"
+                alt="budget balance"
+              />
               <p />
             </li>
           </ul>
